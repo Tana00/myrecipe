47b7ee43da00281627f784031ac8b5e8b64a0676
Happiness Balogun <happinessblgn0@gmail.com>
2019-11-07 18:40:19 +0100

    Develop a recipe website with recipe data from food2fork api

---
+++ app.js
@@ -0,0 +1,31 @@
+function handleSearch() {
+  var div = document.getElementById("cardDiv");
+  const query = $("#searchInput").val();
+  const url =
+    "https://www.food2fork.com/api/search?key=a9f512078036f765d8cb7b8a968864e6&q=" +
+    query;
+  fetch(url)
+    .then(resp => {
+      resp.json().then(data => {
+        console.log(data);
+        return data.recipes.map(function(recipe) {
+          console.log(recipe);
+          div.innerHTML += `
+          <div class = "card col-sm-12 col-md-4 my-3 mx-auto">
+            <div class="img-wrapper">
+              <img class = card-img" src = "${recipe.image_url}">
+            </div>
+            <div class = "card-body">
+              <h5 class = "card-title"><a href="${recipe.source_url}">${recipe.title}</a></h5>
+              <p class = "card-text"><b>Source: </b><a href="${recipe.publisher_url}">${recipe.publisher}</a></p>
+          </div>
+        </div>
+          `;
+        });
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
